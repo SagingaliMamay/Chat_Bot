6b4efc41b0da963e7029e93ccdb52eba4ba67859
simon aerts <simonaertsbecode@gmail.com>
2021-02-16 13:38:57 +0100

    Animation chargement

--- script.js
+++ script.js
@@ -31,13 +31,10 @@ var agressiveAnswer = "Dude, are you sure you went to school ? You can't read.";
 
 var byeByeUser = "Good bye it has been fun !";
 
-<<<<<<< HEAD
 for (let el of document.querySelectorAll(".logo1")) el.style.display = "none";
 
 for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
 
-=======
->>>>>>> ae4d25621803f2c8258b43efd2389bcf9b92bd2f
 function helloOnload() {
   displayMsg1.innerHTML =
     displayMsg1.innerHTML + "Bot: " + randomBotHello + "<br>";
@@ -76,7 +73,6 @@ function sendMessage1() {
   form1.reset();
   if (userMessage.includes("yes") == true) {
     setTimeout(function () {
-<<<<<<< HEAD
       for (let el of document.querySelectorAll(".gif")) el.style.display = "";
     });
     setTimeout(function () {
@@ -94,21 +90,11 @@ function sendMessage1() {
         displayMsg1.innerHTML + "Bot: " + randomNoAnswerVar + "<br>";
       for (let el of document.querySelectorAll(".gif"))
         el.style.display = "none";
-=======
-      displayMsg1.innerHTML =
-        displayMsg1.innerHTML + "Bot: " + randomYesAnswerVar + "<br>";
-    }, 1000);
-  } else if (userMessage.includes("no") == true) {
-    setTimeout(function () {
-      displayMsg1.innerHTML =
-        displayMsg1.innerHTML + "Bot: " + randomNoAnswerVar + "<br>";
->>>>>>> ae4d25621803f2c8258b43efd2389bcf9b92bd2f
     }, 1000);
   } else {
     attemptsTry = attemptsTry + 1;
     if (attemptsTry < 3) {
       setTimeout(function () {
-<<<<<<< HEAD
         for (let el of document.querySelectorAll(".gif")) el.style.display = "";
       });
       setTimeout(function () {
@@ -116,15 +102,10 @@ function sendMessage1() {
           displayMsg1.innerHTML + "Bot: " + neitherNoOrYesVar + "<br>";
         for (let el of document.querySelectorAll(".gif"))
           el.style.display = "none";
-=======
-        displayMsg1.innerHTML =
-          displayMsg1.innerHTML + "Bot: " + neitherNoOrYesVar + "<br>";
->>>>>>> ae4d25621803f2c8258b43efd2389bcf9b92bd2f
       }, 1000);
     }
     if (attemptsTry == 3) {
       setTimeout(function () {
-<<<<<<< HEAD
         for (let el of document.querySelectorAll(".gif")) el.style.display = "";
       });
       setTimeout(function () {
@@ -145,14 +126,6 @@ function sendMessage1() {
           displayMsg1.innerHTML + "Bot: " + byeByeUser + "<br>";
         for (let el of document.querySelectorAll(".gif"))
           el.style.display = "none";
-=======
-        displayMsg1.innerHTML =
-          displayMsg1.innerHTML + "Bot: " + agressiveAnswer + "<br>";
-      }, 2000);
-      setTimeout(function () {
-        displayMsg1.innerHTML =
-          displayMsg1.innerHTML + "Bot: " + byeByeUser + "<br>";
->>>>>>> ae4d25621803f2c8258b43efd2389bcf9b92bd2f
       }, 4000);
       document.getElementById("message2").disabled = true;
     }
