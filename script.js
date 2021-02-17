6cf37059983be82827d691c973e021a9749237aa
simon aerts <simonaertsbecode@gmail.com>
2021-02-17 10:28:16 +0100

    réparation site

--- script.js
+++ script.js
@@ -16,18 +16,6 @@ var randomYesAnswerVar = yesAnswerVar[Math.floor(yesAnswerVar.length * Math.rand
 
 var noAnswerVar = ["Not cool", "I am sad to here that", "I hope you enjoy your time here !"];
 var randomNoAnswerVar = noAnswerVar[Math.floor(noAnswerVar.length * Math.random())];
-var yesAnswerVar = ["Cool", "I am glad to hear that", "You lucky user"];
-
-var randomYesAnswerVar =
-  yesAnswerVar[Math.floor(yesAnswerVar.length * Math.random())];
-
-var noAnswerVar = [
-  "Not cool",
-  "I am sad to hear that",
-  "I hope you enjoy your time here !",
-];
-var randomNoAnswerVar =
-  noAnswerVar[Math.floor(noAnswerVar.length * Math.random())];
 
 var neitherNoOrYesVar = "I didn't understand, can you please try again ?";
 
@@ -46,9 +34,9 @@ var agressiveAnswer = "Dude, are you sure you went to school ? You can't read.";
 
 var byeByeUser = "Good bye it has been fun !";
 
-for (let el of document.querySelectorAll("#logo1")) el.style.display = "none";
+// for (let el of document.querySelectorAll("#logo1")) el.style.display = "none";
 
-for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+// for (let el of document.querySelectorAll(".gif")) el.style.display = "none";  
 
 function helloOnload() {
   displayMsg1.innerHTML =
@@ -82,15 +70,149 @@ function sendMessage1() {
     displayMsg1.innerHTML + "You: " + accessMsg1.value + "<br>";
   var userMessage = accessMsg1.value;
 
+  scrollToBottom("message1");
+  form1.reset();
+  
+  if (userMessage.includes("yes") == true) {
+    setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+    setTimeout(function () {
+      displayMsg1.innerHTML = displayMsg1.innerHTML + "Bot: " + randomYesAnswerVar + "<br>";scrollToBottom("message1");
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+    }, 1000);
+      
+    setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+      setTimeout(function () {
+      displayMsg1.innerHTML = displayMsg1.innerHTML + "Bot: " + randomQuestionBot + "<br>";scrollToBottom("message1");
+        for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+    }, 2000);
+      
+    } 
+    else if (userMessage.includes("no") == true) {
+     setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+      setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + randomQuestionBot + "<br>";scrollToBottom("message1")
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+    }, 2000);
+
+  }else if (userMessage.includes("thanks") == true) {
+    setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + passwordBot + "<br>";scrollToBottom("message1")
+    }, 2000);
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + botThanks + "<br>";scrollToBottom("message1")
+        for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+    }, 2000);}
+    else if (userMessage.includes("thank you") == true) {
+       setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+      setTimeout(function () {
+        displayMsg1.innerHTML =
+          displayMsg1.innerHTML + "Bot: " + passwordBot + "<br>";scrollToBottom("message1")
+          for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+      }, 2000);
+        setTimeout(function () {
+      for (let el of document.querySelectorAll(".gif")) el.style.display = "";
+    });
+      setTimeout(function () {
+        displayMsg1.innerHTML =
+          displayMsg1.innerHTML + "Bot: " + botThanks + "<br>";scrollToBottom("message1")
+          for (let el of document.querySelectorAll(".gif")) el.style.display = "none";
+      }, 2000);}
+  else if(userMessage.includes("cold") == true){
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + botThanks + "<br>";scrollToBottom("message1")
+    }, 2000);
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + botAnswerUserCold + "<br>";scrollToBottom("message1")
+    }, 4000);
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + passwordBot + "<br>";scrollToBottom("message1")
+    }, 5000);
+  } else if(userMessage.includes("hot") == true){
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + botThanks + "<br>";scrollToBottom("message1")
+    }, 2000);
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + botAnswerUserHot + "<br>";scrollToBottom("message1")
+    }, 4000);
+    setTimeout(function () {
+      displayMsg1.innerHTML =
+        displayMsg1.innerHTML + "Bot: " + passwordBot + "<br>";scrollToBottom("message1")
+    }, 5000);
+  } else {
+    attemptsTry = attemptsTry + 1;
+    if (attemptsTry < 3) {
+      setTimeout(function () {
+        displayMsg1.innerHTML =
+          displayMsg1.innerHTML + "Bot: " + neitherNoOrYesVar + "<br>";scrollToBottom("message1")
+      }, 1000);
+
+    } if (attemptsTry == 3) {
+      setTimeout(function () {
+        displayMsg1.innerHTML =
+          displayMsg1.innerHTML + "Bot: " + agressiveAnswer + "<br>";scrollToBottom("message1")
+      }, 2000);
+      setTimeout(function () {
+        displayMsg1.innerHTML =
+          displayMsg1.innerHTML + "Bot: " + byeByeUser + "<br>";scrollToBottom("message1")
+      }, 4000);
+
+      document.getElementById("message2").disabled = true;
+    }
+  }
+}
+
+setTimeout(function () {
+  greetings();
+}, 2000);
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+function scrollToBottom(id) {
+  var div = document.getElementById(id);
+  div.scrollTop = div.scrollHeight - div.clientHeight;
+}
+
+
+form1.addEventListener("submit", handleForm);
+
+function sendMessage1() {
+  displayMsg1.innerHTML =
+    displayMsg1.innerHTML + "You: " + accessMsg1.value + "<br>";
+  var userMessage = accessMsg1.value;
+
   
 
   scrollToBottom("message1");
   form1.reset();
   
   if (userMessage.includes("yes") == true) {
+      setTimeout(function () {
+      });
     setTimeout(function () {
       displayMsg1.innerHTML = displayMsg1.innerHTML + "Bot: " + randomYesAnswerVar + "<br>";scrollToBottom("message1");}, 1000);
-
+      setTimeout(function () {
+      });
     setTimeout(function () {
       displayMsg1.innerHTML = displayMsg1.innerHTML + "Bot: " + randomQuestionBot + "<br>";scrollToBottom("message1")}, 2000);
 
